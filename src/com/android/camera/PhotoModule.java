1ffad96f15671553db8a6c8a9d9d3b0c9fd99f85
codeworkx <codeworkx@cyanogenmod.org>
2016-01-06 00:10:40 +0100

    CameraX: add options to restart preview onPictureTaken
    Change-Id: I7ce9d6f2323e367743e6d4c9662b846d3052f2a4

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -980,6 +980,8 @@ public class PhotoModule
             return;
         }
 
+        mApplicationContext = CameraApp.getContext();
+
         // Initialize location service.
         boolean recordLocation = RecordLocationPreference.get(mPreferences,
                 CameraSettings.KEY_RECORD_LOCATION);
@@ -1518,7 +1520,20 @@ public class PhotoModule
                     && (mCameraState != LONGSHOT)
                     && (mSnapshotMode != CameraInfoWrapper.CAMERA_SUPPORT_MODE_ZSL)
                     && (mReceivedSnapNum == mBurstSnapNum);
+
             needRestartPreview |= mLgeHdrMode;
+
+            boolean backCameraRestartPreviewOnPictureTaken =
+                mApplicationContext.getResources().getBoolean(R.bool.back_camera_restart_preview_onPictureTaken);
+            boolean frontCameraRestartPreviewOnPictureTaken =
+                mApplicationContext.getResources().getBoolean(R.bool.front_camera_restart_preview_onPictureTaken);
+
+            CameraInfo info = CameraHolder.instance().getCameraInfo()[mCameraId];
+            if ((info.facing == CameraInfo.CAMERA_FACING_BACK && backCameraRestartPreviewOnPictureTaken)
+                    || (info.facing == CameraInfo.CAMERA_FACING_FRONT && frontCameraRestartPreviewOnPictureTaken)) {
+                needRestartPreview = true;
+            }
+
             if (needRestartPreview) {
                 mHandler.post(() -> {
                     setupPreview();
@@ -1563,7 +1578,6 @@ public class PhotoModule
                             .findPreference(CameraSettings.KEY_SELFIE_MIRROR);
                     if (selfieMirrorPref != null && selfieMirrorPref.getValue() != null &&
                             selfieMirrorPref.getValue().equalsIgnoreCase("enable")) {
-                        CameraInfo info = CameraHolder.instance().getCameraInfo()[mCameraId];
                         jpegData = flipJpeg(jpegData, info.orientation, orientation);
                         jpegData = addExifTags(jpegData, orientation);
                     }
